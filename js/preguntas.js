040b16121cdaf8d149ae4596809451c1934c62cc
pedrosalazar <pedrosalazarr.25@gmail.com>
2022-12-09 00:29:42 -0500

    añadiendo js de los sliders y preguntas

---
+++ js/preguntas.js
@@ -0,0 +1,21 @@
+(function () {
+    const titlePreguntas = [...document.querySelectorAll('.preguntas_title')];
+    // console.log(titlePreguntas)
+    titlePreguntas.forEach(pregunta => {
+        pregunta.addEventListener('click', () => {
+            let height = 0;
+            let answer = pregunta.nextElementSibling;
+            let addPadding = pregunta.parentElement.parentElement;
+
+            addPadding.classList.toggle('preguntas_padding--add');
+            pregunta.children[0].classList.toggle('preguntas_arrow--rotate');
+
+            if (answer.clientHeight === 0) {
+                height = answer.scrollHeight;
+            }
+
+            answer.style.height = `${height}px`
+        })
+    });
+
+})();
