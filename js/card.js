f720d6fb84e07e6efafb2e47fe4f4362809cff90
pedrosalazar <pedrosalazarr.25@gmail.com>
2022-12-10 23:45:08 -0500

    agregando JSON

--- js/card.js
+++ js/card.js
@@ -1,6 +1,20 @@
 const divCards = document.querySelector('.price_table')
 
-async
+
 const buscarProductos = async () => {
-    const productosFetch = fetch ('')
+    const productosFetch = await fetch('./js/consolas.json')
+    const productosJson = await productosFetch.json()
+    productosJson.forEach(prod => {
+        const { id, price, title, category, image } = prod
+        divCards.innerHTML += `
+        <div class="price_element">
+                    <p class="price_name">${title}</p>
+                    <img src="${image}" alt="" class="price_cards">
+                    <a href="#" class="price_cta">${price}</a>
+                </div>
+        `
+    })
 }
+
+buscarProductos()
+
