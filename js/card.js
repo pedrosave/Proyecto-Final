381b47015a6ec7191d187757fe724586e90b880a
pedrosalazar <pedrosalazarr.25@gmail.com>
2022-12-12 00:24:03 -0500

    haciendo el DOM

--- js/card.js
+++ js/card.js
@@ -1,19 +1,24 @@
 const divCards = document.querySelector('.price_table')
 
-
 const buscarProductos = async () => {
     const productosFetch = await fetch('./js/consolas.json')
     const productosJson = await productosFetch.json()
     productosJson.forEach(prod => {
-        const { id, price, title, category, image } = prod
+        const { id, price, title, image } = prod
         divCards.innerHTML += `
         <div class="price_element">
-                    <p class="price_name">${title}</p>
-                    <img src="${image}" alt="" class="price_cards">
-                    <a href="#" class="price_cta">${price}</a>
-                </div>
-        `
+            <p class="price_name">${title}</p>
+            <img src="${image}" alt="" class="price_cards">
+            <p class="price_items">S/ ${price}</p>
+            <button onclick="agregarProducto(${id})" class="price_finalizar">Agregar al Carrito</button>
+        </div>
+        `        
     })
+    
+}
+
+function agregarProducto(id) {
+    console.log(id)
 }
 
 buscarProductos()
